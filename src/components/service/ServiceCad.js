9ae2a6c74322b75a0ba463ea705e0407c3583d30
evandrofelimberti <evandrofelimberti@gmail.com>
2023-06-28 21:38:40 -0300

    Ajuste layout

--- src/components/service/ServiceCad.js
+++ src/components/service/ServiceCad.js
@@ -1,4 +1,4 @@
-import styles from '../project/ProjectCard.module.css'
+import styles from './ServiceCard.module.css'
 import {BsFillTrashFill} from 'react-icons/bs'
 import { numberFormat } from '../form/numberFormat';
 
@@ -11,12 +11,12 @@ function ServiceCad({id, Descricao, Quantidade, Produto, Valor, Total,  handleRe
     return(
         <div className={styles.project_card}>
             <h4>{Produto}</h4>
-            <p ><span>{Descricao}</span></p>
+           {/* <p ><span>{Descricao}</span></p>*/}
             <p >Quantidade: {new Intl.NumberFormat('pt-BR', { currency: 'BRL' }).format(Quantidade)} </p>
             <p >Valor: {numberFormat(Valor)} </p>
             <p ><span>Total: {numberFormat(Total)} </span> </p>            
             <div className={styles.project_card_actions}>
-                <button onClick={remove}>
+                <button onClick={remove} >
                     <BsFillTrashFill /> Excluir
                 </button>
             </div>
